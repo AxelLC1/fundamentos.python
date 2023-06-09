8938c498cf3025ee3dc3d0e4ea6b38d6b4b86eab
axel <axel.2002.yael@gmail.com>
2023-06-09 13:24:21 -0600

    02 Variables

---
+++ variables.py
@@ -0,0 +1,12 @@
+# Python es npo Tipado
+
+name = "Axel Luna"
+age = 21
+height = 1.80
+is_student = True
+children = []
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
