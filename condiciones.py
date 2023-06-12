189dc424726d94bb3b5de9417041b56beb444b12
axel <axel.2002.yael@gmail.com>
2023-06-12 10:47:21 -0600

    06 Condicionales

---
+++ condiciones.py
@@ -0,0 +1,13 @@
+# Condicionales (if, elf, else)
+
+age = 18
+
+if age >= 18:
+    print("Es mayor de edad")
+
+if age < 18:
+    print("Es menor de edad")
+elif age < 60:
+    print("Es mayor de edad")
+else:
+    print("Es adulto mayor")
