f767dab5ce9f1f2f15b178d3768b2a51687f06d5
axel <axel.2002.yael@gmail.com>
2023-06-16 13:25:39 -0600

    12 Funciones

---
+++ funciones.py
@@ -0,0 +1,33 @@
+# Funciones
+# def name_function(params):
+#   code
+
+# Sin parámetros y sin retorno
+def saluda():
+    print("Hola a Todos!")
+
+saluda()
+
+# Con parametros, sin retorno
+def duplica(num):
+    print(num * 2)
+
+duplica(5)
+
+def suma(num1, num2):
+    print(f"la suma de los numeros es: { num1 + num2}")
+
+suma(23, 45)
+
+# Prametros opcionales, sin retorno
+def get_lista(al_1 = "Jose", al_2 = "Darlen"):
+    return [al_1, al_2]
+
+my_list = get_lista()
+print(my_list)
+my_list = get_lista("Peter")
+print(my_list)
+my_list = get_lista("Peter Parker", "Tony Stark")
+print(my_list)
+my_list = get_lista(al_2="Peter Parker", al_1="Tony Stark")
+print(my_list)
